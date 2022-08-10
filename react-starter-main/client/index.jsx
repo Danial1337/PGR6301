e6f8285b8cadac50bbaf57d1899765cb0c318f9e
needtopassclass <danal_123@hotmail.com>
2022-08-10 10:32:36 +0200

    heroku fucks up

--- react-starter-main/client/index.jsx
+++ react-starter-main/client/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import * as React from "react";
+import {useState, useEffect} from "react";
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom"
 
