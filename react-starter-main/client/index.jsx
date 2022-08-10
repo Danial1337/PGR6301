25694c057cb517ac9f35bc91878891cf03af1ce6
needtopassclass <danal_123@hotmail.com>
2022-08-10 10:20:55 +0200

    import react bug??

--- react-starter-main/client/index.jsx
+++ react-starter-main/client/index.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import {useState, useEffect} from "react";
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom"
