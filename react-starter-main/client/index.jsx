012ba8e89f577af9413b6bd4fcbb4c01ae9793f5
needtopassclass <danal_123@hotmail.com>
2022-08-10 10:22:28 +0200

    enda en fix

--- react-starter-main/client/index.jsx
+++ react-starter-main/client/index.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom"
 
