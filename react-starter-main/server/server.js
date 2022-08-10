72f3d4e8e0b19ec92f133700aabc56238682266a
needtopassclass <danal_123@hotmail.com>
2022-08-10 09:38:35 +0200

    first commit

---
+++ react-starter-main/server/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+
+const app = express();
+
+
+const MOVIES = [
+  {
+      title: "the Matrix -- fra server",
+      plot: "a computer haker learns from mysterious rebels about the...",
+      year: 1999
+  },
+  {
+      title: "The Color Purple",
+      plot: "A black souther woman struggles to find her identity after years of suffering",
+      year: 1985
+  }
+];
+
+app.use(bodyParser.json());
+
+app.get("/api/movies", (req, res) =>{
+res.json(MOVIES)
+});
+
+app.post("/api/movies", (req, res) => {
+  const {title, year, plot} = req.body;
+  MOVIES.push({title,year,plot});
+  res.sendStatus(200);
+})
+
+app.use(express.static(path.resolve("..", "client","dist")));
+app.use((req,res) => {
+  res.sendFile(path.resolve("..","client","dist","index.html"))
+})
+
+const server = app.listen(3000, () => {
+  console.log("listening on http://localhost:" + server.address().port);
+});
